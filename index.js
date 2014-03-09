4e63fc74eb26a159571f4073892e506d9ef3fd8d
Vsevolod Strukchinsky <floatdrop@users.noreply.github.com>
2014-03-09 19:11:39 +0500

    Unused require of non-existent file

--- index.js
+++ index.js
@@ -5,10 +5,9 @@ var gulp       	= require('gulp');
 var gutil       = require('gulp-util');
 var through     = require('through2');
 var fs          = require('fs');
-var util        = require('./lib/util');
-var git 	  	= require('./lib/git');
-var os 			= require('os');
-var when 		= require('when');
+var git 	= require('./lib/git');
+var os 	        = require('os');
+var when        = require('when');
 var PluginError = gutil.PluginError;
 
 /*
