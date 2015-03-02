ec7601ac036657fcb8e76fc091dbfa2e2e7026c9
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2015-03-03 01:25:05 +0900

    use mocha instead of jasmine-node

---
+++ test.js
@@ -0,0 +1,338 @@
+'use strict';
+
+var assert = require('assert');
+var path = require('path');
+
+var File = require('vinyl');
+var fs = require('graceful-fs');
+var ghPages = require('./');
+var git = require('./lib/git');
+var github = require('octonode');
+var logSymbols = require('log-symbols');
+var readRemoveFile = require('read-remove-file');
+var rimraf = require('rimraf');
+var through = require('through2');
+var uuid = require('node-uuid');
+
+var tmpDir = path.join(require('os').tmpdir(), 'tmpRepo');
+var tmpStr = uuid.v4();
+var tmpFile = new File({
+  path: tmpStr,
+  contents: new Buffer(tmpStr)
+});
+var files = [
+  new File({
+    path: '.gitignore',
+    contents: new Buffer('ignored-path')
+  }),
+  new File({
+    path: 'ignored-path',
+    contents: new Buffer('ignored')
+  }),
+  tmpFile
+];
+
+var accessToken = process.env.GH_ACCESS_TOKEN;
+var client;
+
+before(function(done) {
+  if (accessToken) {
+    console.log(
+      logSymbols.success +
+      ' Set Github Access token from the environment variable'
+    );
+    client = github.client(accessToken);
+    done();
+
+  } else {
+    var accessTokenFile = 'gh-access-token.txt';
+
+    fs.readFile(accessTokenFile, 'utf8', function(err, text) {
+      if (err || !text) {
+        console.warn(
+          logSymbols.warning +
+          ' Create a plain text file "' +
+          accessTokenFile +
+          '" which contains your Github access token.'
+        );
+        accessToken = '';
+      } else {
+        accessToken = text.trim();
+        console.log(
+          logSymbols.success +
+          ' Set Github Access token from ' + accessTokenFile
+        );
+      }
+
+      client = github.client(accessToken);
+      done();
+    });
+  }
+});
+
+describe('git operations on a repo', function() {
+  before(function(done) {
+    rimraf(tmpDir, done);
+  });
+
+  it('should throw an error when checking out a non existent branch', function(done) {
+    git.prepareRepo('https://github.com/shinnn/gulp-gh-pages.git')
+    .then(function(repo) {
+      return repo.checkoutBranch('non-existent-branch');
+    })
+    .catch(function(err) {
+      var expectedMsg = 'error: pathspec \'' +
+                        'non-existent-branch' +
+                        '\' did not match any file(s) known to git.\n';
+      assert.notEqual(err.message.indexOf(expectedMsg), -1);
+      done();
+    });
+  });
+});
+
+describe('git operations on special repositories', function() {
+  before(function(done) {
+    rimraf(tmpDir, done);
+  });
+
+  it('should be initialized on the default branch', function(done) {
+    git.prepareRepo('https://github.com/LeaVerou/csss.git')
+    .then(function(repo) {
+      assert.equal(repo._currentBranch, 'gh-pages');
+      done();
+    });
+  });
+});
+
+describe('gulp-gh-pages', function() {
+  before(function(done) {
+    client.del('/repos/shinnn/gulp-gh-pages/git/refs/heads/tmp', {}, function(err) {
+      if (err && err.message !== 'Reference does not exist') {
+        done(err);
+        return;
+      }
+
+      client.post('/repos/shinnn/gulp-gh-pages/git/refs', {
+        ref: 'refs/heads/tmp',
+        sha: '563f1ce4dd22b71a4d3ef459793d9818a4482d70'
+      }, done);
+    });
+  });
+
+  it('should ignore empty vinyl file objects', function(done) {
+    ghPages()
+    .on('error', done)
+    .on('data', function(file) {
+      assert(file.isNull());
+      done();
+    })
+    .end(new File());
+  });
+
+  it('should emit an error when it takes files with stream contents', function(done) {
+    ghPages()
+    .on('error', function(err) {
+      assert.equal(err.message, 'Stream content is not supported');
+      done();
+    })
+    .end(new File({contents: through.obj()}));
+  });
+
+  it('should push commits to an existing gh-pages branch', function(done) {
+    var stream = ghPages({
+      remoteUrl: 'https://' + accessToken + '@github.com/shinnn/gulp-gh-pages.git',
+      branch: 'tmp',
+      message: '[ci skip] temporary commit ("tmp" branch)'
+    })
+    .on('error', done)
+    .on('data', function(file) {
+      assert(file.isBuffer());
+    })
+    .on('end', function() {
+      fs.readFile(path.join(tmpDir, tmpStr), function(err, buf) {
+        assert.strictEqual(err, null);
+        assert.equal(String(buf), tmpStr);
+        done();
+      });
+    });
+
+    files.forEach(function(file) {
+      stream.write(file);
+    });
+
+    stream.end();
+  });
+
+  it('should not push any commits when no files has been changed', function(done) {
+    var stream = ghPages({
+      remoteUrl: 'https://' + accessToken + '@github.com/shinnn/gulp-gh-pages.git',
+      branch: 'tmp'
+    })
+    .on('error', done)
+    .on('data', function(file) {
+      assert(file.isBuffer());
+    })
+    .on('end', done);
+
+    files.forEach(function(file) {
+      stream.write(file);
+    });
+
+    stream.end();
+  });
+
+  it('should create and checkout a branch', function(done) {
+    client.del('/repos/shinnn/gulp-gh-pages/git/refs/heads/new', {}, function(err) {
+      if (err && err.message !== 'Reference does not exist') {
+        done(err);
+        return;
+      }
+
+      var stream = ghPages({
+        remoteUrl: 'https://' + accessToken + '@github.com/shinnn/gulp-gh-pages.git',
+        branch: 'new',
+        message: '[ci skip] temporary commit ("new" branch)',
+        push: true,
+        force: true
+      })
+      .on('error', done)
+      .on('data', function(file) {
+        assert(file.isBuffer());
+      })
+      .on('end', function() {
+        client.del('/repos/shinnn/gulp-gh-pages/git/refs/heads/new', {}, function(reqErr) {
+          if (reqErr && reqErr.message !== 'Reference does not exist') {
+            done(reqErr);
+            return;
+          }
+          done();
+        });
+      });
+
+      stream.write(new File({
+        path: '.gitignore',
+        contents: new Buffer('node_modules\nfoo')
+      }));
+
+      stream.write(new File({
+        path: 'foo',
+        contents: new Buffer('hi')
+      }));
+
+      stream.end(new File({
+        path: 'node_modules/bar.txt',
+        contents: new Buffer('hello\n')
+      }));
+    });
+  });
+
+  it('should specify the cache directory path with `cacheDir` option', function(done) {
+    ghPages({
+      remoteUrl: 'https://' + accessToken + '@github.com/shinnn/gulp-gh-pages.git',
+      cacheDir: '__cache__',
+      push: false
+    })
+    .on('error', done)
+    .on('data', function(file) {
+      assert(file.isBuffer());
+    })
+    .on('end', function() {
+      readRemoveFile(path.join('__cache__', tmpStr), function(readErr, buf) {
+        assert.strictEqual(readErr, null);
+        assert.equal(String(buf), tmpStr);
+        done();
+      });
+    })
+    .end(tmpFile);
+  });
+
+  it('should emit an error when it fails to write files', function(done) {
+    var stream = ghPages({
+      remoteUrl: 'https://' + accessToken + '@github.com/shinnn/gulp-gh-pages.git'
+    })
+    .on('error', function(err) {
+      assert.equal(err.syscall, 'mkdir');
+      done();
+    });
+
+    stream.write(new File({
+      path: 'foo',
+      contents: new Buffer('foo')
+    }));
+
+    stream.end(new File({
+      path: 'foo/bar',
+      contents: new Buffer('bar')
+    }));
+  });
+
+  it('should emit an error when it fails to remove files', function(done) {
+    var stream = ghPages({
+      remoteUrl: 'https://' + accessToken + '@github.com/shinnn/gulp-gh-pages.git',
+      cache: '_df',
+      push: false
+    })
+    .on('error', function(err) {
+      assert(/did not match any files/.test(err.message));
+      done();
+    })
+    .on('data', function() {
+      rimraf.sync('_df');
+    });
+
+    stream.end(tmpFile);
+  });
+
+  it('should emit an error when it fails to create a cache directory', function(done) {
+    ghPages({
+      remoteUrl: 'https://' + accessToken + '@github.com/shinnn/gulp-gh-pages.git',
+      cacheDir: __filename
+    })
+    .on('error', function(err) {
+      assert.equal(err.code, 'ENOTDIR');
+      done();
+    })
+    .end(tmpFile);
+  });
+
+  it('should emit an error when the repository doesn\'t exist', function(done) {
+    ghPages({remoteUrl: 'https://_/_this_/_repo_/_does_/_not_/_exist_/_.git'})
+    .on('error', function(err) {
+      assert(err);
+      done();
+    })
+    .end(tmpFile);
+  });
+
+  it('should emit an error when the user has no permission to push the repo', function(done) {
+    ghPages({remoteUrl: 'https://' + accessToken + '@github.com/bot/move.git'})
+    .on('error', function(err) {
+      assert(err);
+      assert(/Permission to/.test(err.message));
+      done();
+    })
+    .end(tmpFile);
+  });
+
+  it('should emit an error when the remote URL is not a git repository\'s URL', function(done) {
+    ghPages({remoteUrl: 'https://example.org/'})
+    .on('error', function(err) {
+      assert(err);
+      assert(/'https:\/\/example\.org\/' not found/.test(err.message));
+      done();
+    })
+    .end(tmpFile);
+  });
+
+  it('should emit an error when the current directory is not a git repository', function(done) {
+    process.chdir('/');
+
+    ghPages()
+    .on('error', function(err) {
+      assert(err);
+      process.chdir(path.resolve(__dirname, '..'));
+      done();
+    })
+    .end(tmpFile);
+  });
+});
